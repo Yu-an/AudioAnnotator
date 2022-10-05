8cda7a34821c63028085719b0f355807e1020349
Yu’an Yang <yuanyang@Yuans-Air.fios-router.home>
2022-10-05 12:11:08 -0400

    abandoning python

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 from flask import Flask, render_template, request, make_response, redirect, url_for
 import pandas as pd
 import numpy as np
+import json
 
 HOST_NAME = '0.0.0.0'
 HOST_PORT = 81
@@ -16,9 +17,18 @@ def index():
 
   return render_template("csv_table.html", tables=[df.to_html(classes='data', header=False, index = False)])
 
+@app.route("/edited")
+def edited():
+  return render_template("editable.html")
+
+
 @app.route("/table")
 def edit_table():
-  return render_template("table.html")
+  audio_names = [
+    {'row_id':1, 'fname':"Code", 'lname':"With Mark", 'email':"mark@codewithmark.com", 'audio':"010512-4.wav"}, 
+    {'row_id':2, 'fname':"Mary", 'lname':"Moe", 'email':"mary@gmail.com",'audio':"010512-165.wav"},
+    ]
+  return render_template("table.html",audio_names=audio_names)
   #
 
 @app.route('/posts/', methods=('GET', 'POST'))
