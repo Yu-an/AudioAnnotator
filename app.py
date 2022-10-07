0d144fde7531064ba218fca8b2eaea17e1206823
Yu'an <yuanyg@amazon.com>
2022-10-07 15:02:24 -0400

    import data (needs improvement)

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 from flask import Flask, render_template, request, make_response, redirect, url_for
 import pandas as pd
 import numpy as np
-from utils import read_scr_csv, prepare_scr_for_js
+# from utils import read_scr_csv, prepare_scr_for_js
 import json
 
 HOST_NAME = '0.0.0.0'
@@ -13,14 +13,17 @@ app.debug = True
 
 @app.route("/", methods=('GET', 'POST'))
 def TranscriptAnnotator():
-#  df = pd.read_csv("static/data/data.csv")
-#  columns = df.columns()
-  # if request.method == "POST":
-  #   print(request.form)
-#     title = request.form["title"]
-#     content = request.form["content"]
-#     return redirect(url_for("index"))
-  return render_template("transcript_annotator.html", scr_data_dir = "../data/data.csv")
+  df = pd.read_csv("static/data/data.csv")
+  other_col_names = json.dumps([x for x in df.columns.to_list() if x not in ["uttrances", "filename"]])
+  audio_trscpt = json.dumps(["filename", "utterances"])
+  filenames = json.dumps(df["filename"].to_list())
+  transcriptions = json.dumps(df["utterances"].to_list())
+  # data_tba = []
+  # for i in [x for x in df.columns.to_list() if x not in ["utterances", "filename"]]:
+  #     f = df[i].to_list()
+  #     data_tba.append(f)
+  # data_tba = json.dumps(data_tba)
+  return render_template("transcript_annotator.html", other_col_names = other_col_names,audio_trscpt= audio_trscpt, filenames=filenames, transcriptions = transcriptions)
 
 
 @app.route("/index")
---
+++ static/js/ImportAnnotCSV.js
@@ -0,0 +1,7 @@
+
+    
+    const audio_utt = {{ audio_trscpt | safe}};
+    const var_to_annt = {{ other_col_names | safe }};
+    
+    const filenames = {{ filenames | safe}}
+    const transcriptions = {{ transcriptions |safe }};
