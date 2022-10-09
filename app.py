1d3765f5e8c6e93d2855a955f6cc37d8f41eaa8f
Yu’an Yang <yuanyang@Yuans-Air.fios-router.home>
2022-10-09 02:21:16 -0400

    basics of the table done; scaling up next

--- app.py
+++ app.py
@@ -11,18 +11,21 @@ app = Flask(__name__)
 app.debug = True
 
 @app.route("/", methods=('GET', 'POST'))
+# This function renders the annotation main page
+# which extends table_base.html template, and uses bootstrap-table plugin
+# Currently the annotation tool can edit the transcript, and annotate some cols
+# ToDo
+# 1. generalize to unknown variables to be annotated
+# 2. have a seperate datahandler, seperate out data directory
+# 3. audio/data directories intake form
+# 4. Also need to figure out how to save data remotely
 def TranscriptAnnotator():
   df = pd.read_csv("static/data/data.csv")
   df_var = pd.read_csv("static/data/annotation_schema.csv")
   col_toannot = ["record", "filename","utterances"] + df_var.columns.to_list()
   df_toannot = df[col_toannot]
-  data_given = df_toannot.values
-  data_all = df_toannot.to_json()
-  # filenames = json.dumps(df["filename"].to_list())
-  # transcriptions = json.dumps(df["utterances"].to_list())
-  # ,  filenames=filenames, transcriptions = transcriptions,
-
-  return render_template("table_boot.html",col_toannot= json.dumps(col_toannot), data_given = data_given, data_all = data_all )
+  data_all = df_toannot.to_json(orient="records")
+  return render_template("annotation_page.html",col_toannot= json.dumps(col_toannot), data_all = data_all )
 
 @app.route("/t")
 def table():
