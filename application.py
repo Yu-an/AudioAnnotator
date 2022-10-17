bd9e77d9157634b959f26f36855e7fdd58bf4045
Yu’an Yang <yuanyang@Yuans-Air.fios-router.home>
2022-10-17 00:40:29 -0400

    add results dashboard, but need to flesh out

--- application.py
+++ application.py
@@ -13,28 +13,22 @@ HOST_PORT = 81
 application = Flask(__name__)
 application.debug = True
 
-@application.route("/", methods=('GET', 'POST'))
+@application.route("/annotation", methods=('GET', 'POST'))
 # This function renders the annotation main page
 # which extends table_base.html template, and uses bootstrap-table plugin
 # Currently the annotation tool can edit the transcript, and annotate some cols
-# ToDo
-# 1. generalize to unknown variables to be annotated
-# 2. have a seperate datahandler, seperate out data directory
-# 3. audio/data directories intake form
 def transcript_annotator():
-  df = pd.read_csv("static/data/data.csv")
-  df_var = pd.read_csv("static/data/annotation_schema.csv")
-  col_toannot = df.columns.to_list() # this needs to be changed to a json file
+  df = pd.read_csv("static/data/data.csv").fillna("None")
   data_all = df.to_json(orient="records")
   if flask.request.method == 'POST':
     annot_output = json.dumps(request.get_json())
     annot = pd.read_json(annot_output, orient="index")
     annot.to_csv("static/data/output.csv") # this needs to save differently
-  return render_template("annotation_page.html",col_toannot= json.dumps(col_toannot), data_all = data_all )
+  return render_template("annotation_page.html", data_all = data_all )
 
 @application.route("/results", methods=('GET', 'POST'))
 def display_results():
-  # df_results = 
+  df_results = pd.read_csv("static/data/output.csv")
   return render_template("results_dashboard.html")
 
 
