ef3a2a97b902f5b73eb66bddf4947e28112072c5
Yu-an <yuanyg@amazon.com>
2024-01-02 10:38:20 -0500

    update main application

---
+++ annotation/annotation.py
@@ -0,0 +1,123 @@
+from flask import Blueprint, render_template, request,url_for,jsonify, session, flash
+from flask import current_app as app
+import json
+import boto3
+from io import StringIO
+import pickle
+import pandas as pd
+from utils import *
+from database import *
+
+aws_session = boto3.session.Session()
+dynamodb = aws_session.resource('dynamodb', region_name='us-east-1')
+user_table = dynamodb.Table('MSDUserTable')
+data_table = dynamodb.Table('MSDDataTable')
+
+annotation_bp = Blueprint('annotation_bp', __name__,
+    template_folder='templates',
+    static_folder='static')
+
+@annotation_bp.route("/<file_name>", methods=('GET', 'POST'))
+def msd_annot(file_name):
+    msd_annot_url =  url_for('annotation_bp.msd_annot', file_name=file_name)
+    msd_review_url = url_for('annotation_bp.msd_review_annot', file_name=file_name)
+
+    user_id = session.get('user_id')
+    if user_id==None:
+        user_id = 'yuanyg' #for testing
+    # add to existing_passes
+    try:                        
+        annotation_file = check_file_annotation_status(data_table,user_id, file_name)
+        # print(f'no session annotation_file for {attribute};', annotation_file.segments[attribute].to_list())
+        # session['annotation_file'] = pickle.dumps(annotation_file)                        
+    except AnnotationFileNotFoundError as e:
+        input_file = get_input_file_from_db(data_table,file_name)
+        # add existing pass if a new file is claimed
+        input_file.existing_passes = input_file.existing_passes+1
+        print(f'added passes to {file_name}', input_file.existing_passes)            
+        update_dynamodb_table(data_table,input_file.to_dict())
+        annotation_file = prep_input_to_be_output(input_file,user_id)
+        print('reading from input:',annotation_file.segments)         
+    except Exception as e:        
+        raise e        
+    # get audio file
+    try:
+        audio_path = access_audio(annotation_file.audio_path)
+    except Exception as e:
+            raise e
+    # get transcript; transcript is a pandas dataframe    
+    transcript = annotation_file.segments
+    # print(f'loading transcript for {attribute}:',transcript[attribute].to_list())          
+    if request.method == 'POST':       
+        try:            
+            # Process the request data (can we do this with class?)
+            segments = request.get_json()            
+            annotation_file.segments = segments            
+            data_item = annotation_file.to_dict()    
+            update_dynamodb_table(data_table,data_item)
+            annotation_file.segments = pd.DataFrame(segments)
+            # print(f'after post for {attribute};', annotation_file.segments[attribute].to_list())            
+            # If everything is successful, return a JSON response (don't do HTML!)
+            return jsonify({"success": True, "message": "Data saved successfully"})
+        except ClientError as e:            
+            return jsonify({"success": False, "message": "DynamoDB error"}), 500
+        except Exception as e:            
+            return jsonify({"success": False, "message": "Unexpected error"}), 500
+
+
+    return render_template('annotation/annot.html',                       
+                        file_name = file_name,
+                        emotion_attributes=emotion_attributes, 
+                        transcript=transcript,
+                        audio_path=audio_path,
+                        msd_annot_url = msd_annot_url,
+                        msd_review_url = msd_review_url
+                        )
+
+
+@annotation_bp.route("/<file_name>/review", methods=('GET', 'POST'))
+def msd_review_annot(file_name):
+    # page to display all segment labels
+    user_id = session.get('user_id')
+    if user_id==None:
+        user_id = 'yuanyg' #for testing
+    # user_data = pickle.loads(session.get('user_data'))
+    # access annotated file from database
+
+    annotation_file = check_file_annotation_status(data_table,user_id, file_name)
+        
+    # annotation_file = check_file_annotation_status(data_table,user_id, file_name)
+    segments = annotation_file.segments
+    metadata = annotation_file.metadata
+    audio_path = access_audio(annotation_file.audio_path)
+    msd_annot_url =  url_for('annotation_bp.msd_annot', file_name=file_name)
+    msd_review_url = url_for('annotation_bp.msd_review_annot', file_name=file_name)
+    my_tasks_url = url_for('mytasks_bp.list_tasks')
+    if (request.method == 'POST'):
+        try:                           
+            annotated_data = request.get_json()                     
+            # Process the request data (can we do this with class?)  
+            annotation_file.segments = annotated_data['segments']
+            annotation_file.metadata['callType'] = annotated_data['metadata']
+            annotation_file.status = 'done'
+            data_item = annotation_file.to_dict()
+            update_dynamodb_table(data_table,data_item)
+            annotation_file.segments = pd.DataFrame(segments)            
+            # update_dynamodb_table(data_table,input_item)
+            # If everything is successful, return a JSON response (don't do HTML!)
+            return jsonify({"success": True, "message": "Data saved successfully"})
+        except ClientError as e:            
+            return jsonify({"success": False, "message": "DynamoDB error"}), 500
+        except Exception as e:            
+            return jsonify({"success": False, "message": "Unexpected error"}), 500
+    
+    
+    return render_template('annotation/annot_viewall.html',
+                           segments = segments,
+                           file_name=file_name,
+                           metadata = metadata,   
+                           audio_path = audio_path,
+                           msd_annot_url = msd_annot_url,
+                           msd_review_url = msd_review_url,
+                           my_tasks_url = my_tasks_url
+                           )
---
+++ login/login.py
@@ -0,0 +1,59 @@
+import boto3
+from flask import Blueprint,redirect,jsonify,url_for, render_template, session
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, validators
+from utils import *
+import aws_config
+import pickle
+
+login_bp = Blueprint('login_bp', __name__,
+    template_folder='templates',
+    static_folder='static')
+
+aws_session = boto3.session.Session()
+dynamodb = aws_session.resource('dynamodb', region_name=aws_config.AWS_config['region'])
+# user_table = dynamodb.Table('MSDUserTable')
+# data_table = dynamodb.Table('MSDDataTable')
+
+
+class LoginForm(FlaskForm):
+    text_input = StringField('Enter Amazon alias (without @amazon)', [validators.DataRequired()])
+    submit_button = SubmitField('Submit')
+    
+@login_bp.route('/', methods=('GET', 'POST'))
+def login():
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        user_id = form.text_input.data
+        if "@" in user_id:
+            user_id = user_id.split('@')[0]
+        session['user_id'] = user_id
+        try:
+            existing_users = query_dynamodb_table(user_table, 'user_id', user_id)
+            if len(existing_users)!=0:            
+                return redirect(url_for('mytasks_bp.list_tasks'))
+        except:
+            return render_template('login/login.html', form=form, login_failed=True)
+    return render_template('login/login.html', form=form, login_failed=False)
+
+
+@login_bp.route("/welcome", methods=('GET', 'POST'))
+def home():
+  # user_id = request.remote_user
+  user_data = pickle.load(session.get('user_data'))
+  first_name = user_data.first_name
+  if first_name==None:
+      first_name = session.get('user_id')
+  return render_template("home.html",
+                         first_name = first_name
+                         )
+
+
+@login_bp.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('login_bp.login'))
+
+
+# use flask-wtf and flask-login?
