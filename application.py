ed9330389fe4a9a904bfdeb7a01d22060356f8c2
Yu-an <yuanyg@amazon.com>
2024-01-02 12:45:58 -0500

    update main app;

--- application.py
+++ application.py
@@ -1,35 +1,32 @@
-from flask import Flask, render_template, request, make_response, redirect, url_for
-import flask
+from flask import Flask, render_template, request,session
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
 import pandas as pd
 import numpy as np
-# from utils import read_scr_csv, prepare_scr_for_js
 import json
+import boto3
+from database import *
+from aws_config import *
+from login.login import login_bp
+from annotation.annotation import annotation_bp
 
-# HOST_NAME = '0.0.0.0'
-# HOST_PORT = 81
 # Elastic Beanstalk doesn't accept 'app', switching to 'application'
 application = Flask(__name__)
-application.debug = True
-@application.route("/", methods=('GET', 'POST'))
+application.debug = False
 
 
-@application.route("/annotation", methods=('GET', 'POST'))
-# This function renders the annotation main page
-# which extends table_base.html template, and uses bootstrap-table plugin
-# Currently the annotation tool can edit the transcript, and annotate some cols
-def transcript_annotator():
-  df = pd.read_csv("static/data/data.csv").fillna("None")
-  data_all = df.to_json(orient="records")
-  if flask.request.method == 'POST':
-    annot_output = json.dumps(request.get_json())
-    annot = pd.read_json(annot_output, orient="index")
-    annot.to_csv("static/data/output.csv") # this needs to save differently
-  return render_template("annotation_page.html", data_all = data_all )
+application.config.from_object('config')
+# dynamotable and possibly s3
+aws_session = boto3.session.Session()
+dynamodb = aws_session.resource('dynamodb', region_name=aws_config.AWS_config['region'])
+# user_table = dynamodb.Table('MSDUserTable')
+# data_table = dynamodb.Table('MSDDataTable')
 
-@application.route("/results", methods=('GET', 'POST'))
-def display_results():
-  df_results = pd.read_csv("static/data/output.csv")
-  return render_template("results_dashboard.html")
+
+application.register_blueprint(login_bp, url_prefix='/')
+application.register_blueprint(annotation_bp, url_prefix='/annot')
+
+
 
 
 if __name__ == "__main__":
