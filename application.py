6299c53b423a29c8a8b5f4333e51aefdf696d061
Yu-an <yuanyg@amazon.com>
2024-01-02 09:45:01 -0500

    modify for prosody project

--- application.py
+++ application.py
@@ -1,5 +1,3 @@
-
-from email.mime import application
 from flask import Flask, render_template, request, make_response, redirect, url_for
 import flask
 import pandas as pd
@@ -7,11 +5,13 @@ import numpy as np
 # from utils import read_scr_csv, prepare_scr_for_js
 import json
 
-HOST_NAME = '0.0.0.0'
-HOST_PORT = 81
+# HOST_NAME = '0.0.0.0'
+# HOST_PORT = 81
 # Elastic Beanstalk doesn't accept 'app', switching to 'application'
 application = Flask(__name__)
 application.debug = True
+@application.route("/", methods=('GET', 'POST'))
+
 
 @application.route("/annotation", methods=('GET', 'POST'))
 # This function renders the annotation main page
@@ -33,4 +33,4 @@ def display_results():
 
 
 if __name__ == "__main__":
-  application.run(HOST_NAME, HOST_PORT)
+  application.run()
--- static/js/ImportAnnotCSV.js
+++
@@ -1,7 +0,0 @@
-
-    
-    const audio_utt = {{ audio_trscpt | safe}};
-    const var_to_annt = {{ other_col_names | safe }};
-    
-    const filenames = {{ filenames | safe}}
-    const transcriptions = {{ transcriptions |safe }};
